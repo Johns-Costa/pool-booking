2e022569ef1d610252a55b5c58cbf2df5feff2cf
joao_cc <joao_cc@yahoo.com>
2024-02-29 17:42:09 +0100

    make messages show up when needed

--- booking/views.py
+++ booking/views.py
@@ -6,7 +6,7 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.core.exceptions import ValidationError
-
+from django.utils import timezone
 # Create your views here.
 
 @login_required
@@ -14,6 +14,15 @@ def index(request):
     # Exclude canceled classes from the queryset
     classes = Class.objects.filter(canceled=False)
     bookings = Booking.objects.all()
+    bookings = Booking.objects.order_by('date_time')
+    current_datetime = timezone.now()
+    bookings = Booking.objects.filter(date_time__gte=current_datetime)
+    bookings_to_cancel = Booking.objects.filter(date_time__lt=current_datetime)
+    
+    # Iterate through the filtered bookings and delete them
+    for booking in bookings_to_cancel:
+        booking.delete()
+    
     return render(request, 'booking/index.html', {'classes': classes, 'bookings': bookings})
 
 @login_required
@@ -25,12 +34,12 @@ def book_class(request):
             # Set the user before saving the form
             form.instance.user = request.user
             form.save()
-            messages.success(request, 'Booking successful!')
-            return redirect('index')
-        except ValidationError as e:
-                messages.error(request, str('Cannot book classes in the past.'))
-        except ValueError as e:
-            messages.error(request, 'Form is not valid. Please check your inputs.')
+            messages.success(request, 'Booking successful!', extra_tags='booking_form')
+            return redirect('book_class')
+        except ValidationError:
+                messages.error(request, 'Cannot book classes in the past.', extra_tags='booking_form')
+        except ValueError:
+            messages.error(request, 'Form is not valid. Please check your inputs.', extra_tags='booking_form')
     else:
         form = BookingForm()
 
@@ -49,10 +58,10 @@ def add_class(request):
 
         if class_name and class_description:
             Class.objects.create(name=class_name, description=class_description)
-            messages.success(request, 'Class added successfully!')
+            messages.success(request, 'Class added successfully!', extra_tags='manage_classes')
             return redirect('manage_classes')
         else:
-            messages.error(request, 'Both class name and description are required.')
+            messages.error(request, 'Both class name and description are required.', extra_tags='manage_classes')
 
     return HttpResponse('Invalid request.')
 
@@ -62,7 +71,7 @@ def cancel_class(request, class_id):
 
     if request.method == 'POST':
         my_class.delete()  # Delete the class permanently
-        messages.success(request, 'Class deleted successfully!')
+        messages.success(request, 'Class deleted successfully!', extra_tags='cancel_class')
         return redirect('manage_classes')
 
     return render(request, 'booking/cancel_class.html', {'class': my_class})
@@ -75,7 +84,8 @@ def edit_booking(request, booking_id):
         form = BookingForm(request.POST, instance=booking)
         if form.is_valid():
             form.save()
-            return redirect('index')  # Redirect to the main page after editing
+            messages.success(request, 'Booking edited successfully!', extra_tags='edit_booking')
+            return redirect('index')
     else:
         form = BookingForm(instance=booking)
 
@@ -86,8 +96,8 @@ def cancel_booking(request, booking_id):
     booking = get_object_or_404(Booking, id=booking_id, user=request.user)
 
     if request.method == 'POST':
-        # Implement logic to handle cancellation (e.g., set a 'canceled' flag)
-        booking.delete()  # This is just an example; adjust based on your requirements
-        return redirect('index')  # Redirect to the main page after cancellation
+        booking.delete()  
+        messages.success(request, 'Booking deleted successfully!', extra_tags='cancel_booking')
+        return redirect('index')
 
     return render(request, 'booking/cancel_booking.html', {'booking': booking})
